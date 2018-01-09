2d4f7f68bc8d52d24d8c5613dea7d11f00754fce
Cernan Bernardo <cernanb@gmail.com>
2018-01-08 23:54:19 -0800

    Add landing page with background

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, ImageBackground, Platform } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ImageBackground
+          source={require('./assets/background.jpg')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              TodoList Geo
+            </Text>
+            <Text style={{ color: '#bdc3c7' }}>Location based to do list</Text>
+          </View>
+        </ImageBackground>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E'
   },
-});
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
+  },
+  largeText: {
+    fontSize: 44,
+    color: '#ecf0f1'
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white'
+  }
+})
